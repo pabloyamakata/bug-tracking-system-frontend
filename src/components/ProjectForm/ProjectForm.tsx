8ba22085c6fa77e684be66f88c8f0212a0be6280
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-02-02 13:33:02 -0300

    ProjectForm component added.

--- src/components/ProjectForm/ProjectForm.tsx
+++ src/components/ProjectForm/ProjectForm.tsx
@@ -4,15 +4,15 @@ import Grid from "@mui/material/Grid";
 import MenuItem from "@mui/material/MenuItem";
 import Typography from "@mui/material/Typography";
 
-function BugForm() {
+function ProjectForm() {
     return(
         <Box sx={{paddingBottom: '30px'}}>
             <Typography 
             variant="h5" 
             sx={{margin: '25px 0 0 20px'}}>
-            Report Bug
+            New Project
             </Typography>
-            
+        
             <Box component='form'>
                 <Grid container>
                     <CustomGrid item xs={12} sm={6}>
@@ -28,41 +28,30 @@ function BugForm() {
                         rows={3} 
                         multiline />
                         <CustomTextField 
-                        label="Project" 
-                        variant="outlined" 
-                        size="small" 
-                        defaultValue={""} 
-                        select>
-                            <MenuItem value='Project 1'>Project 1</MenuItem>
-                            <MenuItem value='Project 2'>Project 2</MenuItem>
-                            <MenuItem value='Project 3'>Project 3</MenuItem>
-                        </CustomTextField>
-                        <CustomTextField 
+                        type="text"
                         label="Project Leader" 
                         variant="outlined" 
                         size="small" />
-                    </CustomGrid>
-
-                    <CustomGrid item xs={12} sm={6}>
-                        <CustomTextField 
-                        label="Urgency Level" 
-                        variant="outlined" 
-                        size="small" 
-                        defaultValue={""} 
-                        select>
-                            <MenuItem value='Low'>Low</MenuItem>
-                            <MenuItem value='Medium'>Medium</MenuItem>
-                            <MenuItem value='High'>High</MenuItem>
-                        </CustomTextField>
                         <CustomTextField 
                         label="Current State" 
                         variant="outlined" 
-                        size="small" 
-                        defaultValue={""} 
+                        size="small"
+                        defaultValue={""}
                         select>
                             <MenuItem value='Pending'>Pending</MenuItem>
-                            <MenuItem value='Solved'>Solved</MenuItem>
+                            <MenuItem value='Finished'>Completed</MenuItem>
                         </CustomTextField>
+                    </CustomGrid>
+        
+                    <CustomGrid item xs={12} sm={6}>
+                        <CustomTextField 
+                        label="Start Date" 
+                        variant="outlined" 
+                        size="small" />
+                        <CustomTextField
+                        label="Deadline" 
+                        variant="outlined" 
+                        size="small" />
                         <CustomTextField 
                         label="Outlined" 
                         variant="outlined" 
@@ -78,4 +67,4 @@ function BugForm() {
     )
 }
 
-export default BugForm;
+export default ProjectForm;
