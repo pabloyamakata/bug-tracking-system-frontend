ae2df487f4ad3160e10dc9cd7a4981adaedda280
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-02-12 22:56:54 -0300

    Formik and Yup packages were installed. ProjectForm component still needs validation with these packages.

--- src/components/Forms/BugForm.tsx
+++ src/components/Forms/BugForm.tsx
@@ -1,3 +1,6 @@
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+
 import {
     CustomPaper,
     CustomGrid, 
@@ -12,6 +15,38 @@ import MenuItem from '@mui/material/MenuItem';
 import Typography from '@mui/material/Typography';
 
 function BugForm() {
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            description: '',
+            project: '',
+            projectLeader: '',
+            currentStatus: '',
+            priorityLevel: '',
+            severityLevel: '',
+            initialDate: '',
+            finalDate: ''
+        },
+        validationSchema: yup.object({
+            name: yup.string()
+            .min(3, 'Name must contain between 3 and 20 characters')
+            .max(20, 'Name must contain between 3 and 20 characters')
+            .required('Name is required'),
+            description: yup.string()
+            .max(100, 'Description must be 100 characters or less')
+            .required('Description is required'),
+            project: yup.string().required('A project is required'),
+            projectLeader: yup.string()
+            .max(60, "Name of project leader must be 60 characters or less")
+            .required('A project leader is required'),
+            currentStatus: yup.string().required("Bug current status is required"),
+            priorityLevel: yup.string().required('Priority level required'),
+            severityLevel: yup.string().required('Severity level required'),
+            initialDate: yup.date().required('Initial date is required'),
+            finalDate: yup.date()
+        }),
+        onSubmit: () => console.log('BugForm submitted!')
+    });
     return(
         <CustomPaper elevation={0}>
             <Typography 
@@ -20,22 +55,69 @@ function BugForm() {
             Report Bug
             </Typography>
             
-            <Box component='form'>
+            <Box 
+            component='form'
+            onSubmit={formik.handleSubmit}>
                 <Grid container>
                     <CustomGrid item xs={12} sm={6}>
                         <CustomTextField 
                         type='text'
-                        label='Name' 
+                        label='Name'
+                        name='name'
+                        error={
+                            formik.touched.name &&
+                            formik.errors.name ?
+                            true : false
+                        }
+                        helperText={
+                            formik.touched.name &&
+                            formik.errors.name ?
+                            formik.errors.name :
+                            null 
+                        }
+                        value={formik.values.name} 
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
                         variant='outlined' 
                         size='small' />
                         <CustomTextField 
-                        label='Description' 
+                        label='Description'
+                        name='description'
+                        error={
+                            formik.touched.description &&
+                            formik.errors.description ?
+                            true : false
+                        }
+                        helperText={
+                            formik.touched.description &&
+                            formik.errors.description ?
+                            formik.errors.description :
+                            null 
+                        }
+                        value={formik.values.description} 
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur} 
                         variant='outlined' 
                         size='small' 
                         rows={3} 
                         multiline />
                         <CustomTextField 
-                        label='Project' 
+                        label='Project'
+                        name='project'
+                        error={
+                            formik.touched.project &&
+                            formik.errors.project ?
+                            true : false
+                        }
+                        helperText={
+                            formik.touched.project &&
+                            formik.errors.project ?
+                            formik.errors.project :
+                            null 
+                        } 
+                        value={formik.values.project} 
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
                         variant='outlined' 
                         size='small' 
                         defaultValue={''} 
@@ -45,11 +127,41 @@ function BugForm() {
                             <MenuItem value='Project 3'>Project 3</MenuItem>
                         </CustomTextField>
                         <CustomTextField 
-                        label='Project Leader' 
+                        label='Project Leader'
+                        name='projectLeader'
+                        error={
+                            formik.touched.projectLeader &&
+                            formik.errors.projectLeader ?
+                            true : false
+                        }
+                        helperText={
+                            formik.touched.projectLeader &&
+                            formik.errors.projectLeader ?
+                            formik.errors.projectLeader :
+                            null 
+                        }
+                        value={formik.values.projectLeader} 
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur} 
                         variant='outlined' 
                         size='small' />
                         <CustomTextField 
                         label='Current Status' 
+                        name='currentStatus'
+                        error={
+                            formik.touched.currentStatus &&
+                            formik.errors.currentStatus ?
+                            true : false
+                        }
+                        helperText={
+                            formik.touched.currentStatus &&
+                            formik.errors.currentStatus ?
+                            formik.errors.currentStatus :
+                            null 
+                        }
+                        value={formik.values.currentStatus} 
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
                         variant='outlined' 
                         size='small' 
                         defaultValue={''} 
@@ -61,40 +173,106 @@ function BugForm() {
 
                     <CustomGrid item xs={12} sm={6}>
                         <CustomTextField 
-                        label='Priority Level' 
+                        label='Priority Level'
+                        name='priorityLevel'
+                        error={
+                            formik.touched.priorityLevel &&
+                            formik.errors.priorityLevel ?
+                            true : false
+                        }
+                        helperText={
+                            formik.touched.priorityLevel &&
+                            formik.errors.priorityLevel ?
+                            formik.errors.priorityLevel :
+                            null 
+                        }
+                        value={formik.values.priorityLevel} 
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur} 
                         variant='outlined' 
                         size='small' 
                         defaultValue={''} 
                         select>
-                            <MenuItem value='Low'>High</MenuItem>
-                            <MenuItem value='High'>Low</MenuItem>
+                            <MenuItem value='High'>High</MenuItem>
+                            <MenuItem value='Moderate'>Moderate</MenuItem>
+                            <MenuItem value='Low'>Low</MenuItem>
                         </CustomTextField>
                         <CustomTextField 
                         label='Severity Level' 
+                        name='severityLevel'
+                        error={
+                            formik.touched.severityLevel &&
+                            formik.errors.severityLevel ?
+                            true : false
+                        }
+                        helperText={
+                            formik.touched.severityLevel &&
+                            formik.errors.severityLevel ?
+                            formik.errors.severityLevel :
+                            null 
+                        }
+                        value={formik.values.severityLevel} 
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
                         variant='outlined' 
                         size='small' 
                         defaultValue={''} 
                         select>
-                            <MenuItem value='Low'>High</MenuItem>
-                            <MenuItem value='High'>Low</MenuItem>
+                            <MenuItem value='High'>High</MenuItem>
+                            <MenuItem value='Moderate'>Moderate</MenuItem>
+                            <MenuItem value='Low'>Low</MenuItem>
                         </CustomTextField>
                         <CustomTextField 
+                        type='date'
                         label='Initial Date' 
+                        name='initialDate'
+                        error={
+                            formik.touched.initialDate &&
+                            formik.errors.initialDate ?
+                            true : false
+                        }
+                        helperText={
+                            formik.touched.initialDate &&
+                            formik.errors.initialDate ?
+                            formik.errors.initialDate :
+                            null 
+                        }
+                        value={formik.values.initialDate} 
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
                         variant='outlined' 
                         size='small' />
                         <CustomTextField 
-                        label='Final Date' 
+                        type='date'
+                        label='Final Date'
+                        name='finalDate'
+                        error={
+                            formik.touched.finalDate &&
+                            formik.errors.finalDate ?
+                            true : false
+                        }
+                        helperText={
+                            formik.touched.finalDate &&
+                            formik.errors.finalDate ?
+                            formik.errors.finalDate :
+                            null 
+                        }
+                        value={formik.values.finalDate} 
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
                         variant='outlined' 
                         size='small' />
 
                         <ButtonContainer>
                             <CustomButton 
+                            type='submit'
                             variant="contained" 
                             color='primary'>
                             Save
                             </CustomButton>
                             <CustomButton
-                            variant='contained'>
+                            variant='contained'
+                            onClick={formik.handleReset}>
                             Reset
                             </CustomButton>
                         </ButtonContainer>
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,13 +1,14 @@
 import { useState } from 'react';
-
 import { useNavigate } from 'react-router-dom';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 
 import {
-    MainContainer, 
-    LoginContainer, 
-    Title, 
-    LoginTextField, 
-    SubmitButton
+  MainContainer, 
+  LoginForm,
+  Title, 
+  LoginTextField, 
+  SubmitButton
 } from './Styles';
 
 import InputAdornment from '@mui/material/InputAdornment';
@@ -17,72 +18,110 @@ import KeyIcon from '@mui/icons-material/Key';
 import KeyOffIcon from '@mui/icons-material/KeyOff';
 
 function Login() {
-    const [isPasswordVisible, setIsPasswordVisible] = useState<boolean>(false);
-    const navigate = useNavigate();
+  const [isPasswordVisible, setIsPasswordVisible] = useState<boolean>(false);
+  const navigate = useNavigate();
 
-    const handlePasswordVisibility = () => {
-      setIsPasswordVisible(!isPasswordVisible);
-    };
-    const handleLoginSubmit = (event: React.FormEvent<HTMLDivElement>) => {
-      event.preventDefault();
-      navigate('/dashboard');    
-    };
-    return(
-        <MainContainer>
-            <LoginContainer 
-            component='form' 
-            onSubmit={
-              event => handleLoginSubmit(event)
-            }>
-                <Title variant='h5'>Sign In</Title>
-                
-                <LoginTextField
-                type='text'
-                label='Username'
-                name='username'
-                autoComplete='off' 
-                variant='outlined' 
-                size='small' 
-                InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="end">
-                        <PersonIcon sx={{color: 'var(--primary-color)'}} />
-                      </InputAdornment>
-                    )
-                }} />
-                
-                <LoginTextField
-                type={isPasswordVisible ? 'text' : 'password'}
-                label='Password'
-                name='password'
-                autoComplete='off'
-                variant='outlined' 
-                size='small'
-                InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="end">
-                        <IconButton 
-                        onClick={handlePasswordVisibility}
-                        sx={{padding: 0}}>
-                          {
-                            isPasswordVisible ? 
-                            <KeyOffIcon sx={{color: 'var(--primary-color)'}} /> : 
-                            <KeyIcon sx={{color: 'var(--primary-color)'}} /> 
-                          }
-                        </IconButton>
-                      </InputAdornment>
-                    )
-                }} />
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: ''
+    },
+    validationSchema: yup.object({
+      username: yup.string()
+      .min(3, 'Username must contain between 3 and 30 characters')
+      .max(30, 'Username must contain between 3 and 30 characters')
+      .required('Username is required'),
+      password: yup.string()
+      .min(3, 'Password must contain between 3 and 60 characters')
+      .max(60, 'Password must contain between 3 and 60 characters')
+      .required('Password is required')
+    }),
+    onSubmit: () => navigate('/dashboard') 
+  });
+    
+  const handlePasswordVisibility = () => {
+    setIsPasswordVisible(!isPasswordVisible);
+  };
+  return(
+    <MainContainer>
+      <LoginForm onSubmit={formik.handleSubmit}>
+        <Title variant='h5'>Sign In</Title>
 
-                <SubmitButton
-                type='submit'
-                variant='contained'
-                size='medium'>
-                Login
-                </SubmitButton>
-            </LoginContainer>
-        </MainContainer>
-    )
+        <LoginTextField
+        type='text'
+        label='Username'
+        name='username'
+        error={
+          formik.touched.username &&
+          formik.errors.username ?
+          true : false
+        }
+        helperText={
+          formik.touched.username &&
+          formik.errors.username ?
+          formik.errors.username :
+          null 
+        }
+        value={formik.values.username}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        autoComplete='off' 
+        variant='outlined' 
+        size='small'
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <PersonIcon sx={{color: 'var(--primary-color)'}} />
+            </InputAdornment>
+          )
+        }} />
+
+        <LoginTextField
+        type={isPasswordVisible ? 'text' : 'password'}
+        label='Password'
+        name='password'
+        error={
+          formik.touched.password &&
+          formik.errors.password ?
+          true : false
+        }
+        helperText={
+          formik.touched.password &&
+          formik.errors.password ?
+          formik.errors.password :
+          null 
+        }
+        value={formik.values.password}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        autoComplete='off'
+        variant='outlined' 
+        size='small'
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton 
+              onClick={handlePasswordVisibility}
+              sx={{padding: 0}}>
+                {
+                  isPasswordVisible ? 
+                  <KeyOffIcon sx={{color: 'var(--primary-color)'}} /> : 
+                  <KeyIcon sx={{color: 'var(--primary-color)'}} /> 
+                }
+              </IconButton>
+            </InputAdornment>
+            )
+        }} />
+
+        <SubmitButton
+        type='submit'
+        variant='contained'
+        size='medium'>
+        Login
+        </SubmitButton>
+      </LoginForm>
+    </MainContainer>
+  )
 }
 
 export default Login;
