6bc892e220ef384ef94f78a466e9247ed95bd252
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-02-13 22:28:54 -0300

    Frontend form validation was completed using Formik and Yup. Next step: make a database connection using PHP and MySQL.

--- src/components/Reports/Projects.tsx
+++ src/components/Reports/Projects.tsx
@@ -14,38 +14,38 @@ import DeleteIcon from '@mui/icons-material/Delete';
 const createData = (
     number: number,
     name: string, 
-    project: string, 
     projectLeader: string, 
     currentStatus: string, 
-    priority: string,
-    severity: string,
-    initialDate: string,
-    finalDate: string,
+    startDate: string, 
+    deadLine: string,
+    frontend: string,
+    backend: string,
+    database: string,
     description: string
 ) => ({
     number,
     name,
-    project,
     projectLeader,
     currentStatus,
-    priority,
-    severity,
-    initialDate,
-    finalDate,
+    startDate,
+    deadLine,
+    frontend,
+    backend,
+    database,
     description
 });
 
 const rows = [
     createData(
         1, 
-        'Bug 1', 
         'Project 1', 
         'Barry Allen', 
         'Pending', 
-        'High', 
-        'Low', 
-        '20/02/2020', 
-        '', 
+        '19/01/2019', 
+        '20/04/2020', 
+        'React', 
+        'PHP', 
+        'MySQL', 
         'See'
     )
 ];
@@ -59,13 +59,13 @@ function Projects() {
                         <TableRow>
                             <TableCell>No.</TableCell>
                             <TableCell>Name</TableCell>
-                            <TableCell>Project</TableCell>
                             <TableCell>Leader</TableCell>
                             <TableCell>Status</TableCell>
-                            <TableCell>Priority</TableCell>
-                            <TableCell>Severity</TableCell>
-                            <TableCell>Start</TableCell>
-                            <TableCell>End</TableCell>
+                            <TableCell>Start Date</TableCell>
+                            <TableCell>Deadline</TableCell>
+                            <TableCell>Frontend</TableCell>
+                            <TableCell>Backend</TableCell>
+                            <TableCell>Database</TableCell>
                             <TableCell>Description</TableCell>
                             <TableCell>Edit</TableCell>
                             <TableCell>Delete</TableCell>
@@ -78,13 +78,13 @@ function Projects() {
                             sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
                                 <TableCell component="th" scope="row">{row.number}</TableCell>
                                 <TableCell>{row.name}</TableCell>
-                                <TableCell>{row.project}</TableCell>
                                 <TableCell>{row.projectLeader}</TableCell>
                                 <TableCell>{row.currentStatus}</TableCell>
-                                <TableCell>{row.priority}</TableCell>
-                                <TableCell>{row.severity}</TableCell>
-                                <TableCell>{row.initialDate}</TableCell>
-                                <TableCell>{row.finalDate}</TableCell>
+                                <TableCell>{row.startDate}</TableCell>
+                                <TableCell>{row.deadLine}</TableCell>
+                                <TableCell>{row.frontend}</TableCell>
+                                <TableCell>{row.backend}</TableCell>
+                                <TableCell>{row.database}</TableCell>
                                 <TableCell>{row.description}</TableCell>
                                 <TableCell>
                                     <IconButton sx={{padding: 0}}>
