27fcfc2377657febf221217c5999d950c5d840c5
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-02-15 22:10:40 -0300

    Need to search for a better alternative to 'any' type in App.tsx component. Then, we can start developing php backend.

--- src/components/Registration/Registration.tsx
+++ src/components/Registration/Registration.tsx
@@ -1,10 +1,13 @@
-import { useState } from 'react';
+import { useContext } from 'react';
+import { AppContext } from '../App/App';
+
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 
 import {
     MainWrapper,
     RegistrationForm,
-    RegistrationField,
-    RegistrationButton 
+    RegistrationField
 } from './Styles';
 
 import InputAdornment from '@mui/material/InputAdornment';
@@ -12,20 +15,54 @@ import PersonIcon from '@mui/icons-material/Person';
 import IconButton from '@mui/material/IconButton';
 import KeyIcon from '@mui/icons-material/Key';
 import KeyOffIcon from '@mui/icons-material/KeyOff';
+import Button from '@mui/material/Button';
 
 function Registration() {
-    const [isPasswordVisible, setIsPasswordVisible] = useState<boolean>(false);
+    const { state, state: { isPasswordVisible }, setState } = useContext(AppContext);
+
+    const formik = useFormik({
+        initialValues: {
+            username: '',
+            password: ''
+        },
+        validationSchema: yup.object({
+            username: yup.string()
+            .min(3, 'Username must contain between 3 and 30 characters')
+            .max(30, 'Username must contain between 3 and 30 characters')
+            .required('Username is required'),
+            password: yup.string()
+            .min(3, 'Password must contain between 3 and 60 characters')
+            .max(60, 'Password must contain between 3 and 60 characters')
+            .required('Password is required')
+        }),
+        onSubmit: () => console.log('Registration form submitted')
+    });
 
     const handlePasswordVisibility = () => {
-        setIsPasswordVisible(!isPasswordVisible);
+        setState({ ...state, isPasswordVisible: !isPasswordVisible });
     };
     return(
         <MainWrapper>
-            <RegistrationForm>
+            <RegistrationForm 
+            onSubmit={formik.handleSubmit}>
                 <RegistrationField
                 type='text'
                 label='Username'
                 name='username'
+                error={
+                    formik.touched.username &&
+                    formik.errors.username ?
+                    true : false
+                }
+                helperText={
+                    formik.touched.username &&
+                    formik.errors.username ?
+                    formik.errors.username :
+                    null
+                }
+                value={formik.values.username}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
                 autoComplete='off' 
                 variant='outlined' 
                 size='small'
@@ -41,6 +78,20 @@ function Registration() {
                 type={isPasswordVisible ? 'text' : 'password'}
                 label='Password'
                 name='password'
+                error={
+                    formik.touched.password &&
+                    formik.errors.password ?
+                    true : false
+                }
+                helperText={
+                    formik.touched.password &&
+                    formik.errors.password ?
+                    formik.errors.password :
+                    null 
+                }
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
                 autoComplete='off'
                 variant='outlined' 
                 size='small'
@@ -60,12 +111,13 @@ function Registration() {
                     )
                 }} />
                     
-                <RegistrationButton
+                <Button
+                sx={{ mt: 3 }}
                 type='submit'
                 variant='contained'
                 size='medium'>
                 Register
-                </RegistrationButton>
+                </Button>
             </RegistrationForm>
         </MainWrapper>
     )
--- src/components/Registration/Styles.ts
+++ src/components/Registration/Styles.ts
@@ -1,20 +1,54 @@
 import Box from "@mui/material/Box";
 import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
 import { styled } from '@mui/material';
 
-export const MainWrapper = styled(Box)({
+import bg_registration from '../../assets/bg-registration.jpg';
 
+export const MainWrapper = styled(Box)({
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    minHeight: '100vh',
+    paddingLeft: '20px',
+    paddingRight: '20px',
+    backgroundImage: `url(${bg_registration})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundAttachment: 'fixed',
+    backgroundRepeat: 'no-repeat'
 });
 
 export const RegistrationForm = styled('form')({
-
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignIntems: 'center',
+    width: '100%',
+    maxWidth: '380px',
+    height: 'auto',
+    padding: '20px',
+    backgroundColor: 'rgba(0, 0, 0, .6)',
+    borderRadius: '7px'
 });
 
-export const RegistrationField = styled(TextField)({
-
-});
-
-export const RegistrationButton = styled(Button)({
-
-});
+export const RegistrationField = styled(TextField)(({ theme }) => ({
+    '.MuiOutlinedInput-input': {
+        color: theme.palette.secondary.contrastText
+    },
+    '.MuiInputLabel-root': {
+        color: theme.palette.secondary.contrastText
+    },
+    '.MuiOutlinedInput-root': {
+        '& fieldset': {
+          borderColor: 'rgba(250, 250, 250, .5)',
+          transition: '.1s ease'
+        },
+        '&:hover fieldset': {
+          borderWidth: '2px',
+          borderColor: theme.palette.primary.main
+        }
+    },
+    '&:nth-of-type(2)': {
+        marginTop: '20px'
+    }
+}));
