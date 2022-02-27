ff91092d7855ff240f6970f14a5ebc7e97863548
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-02-26 23:02:39 -0300

    ...

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,4 @@
-import { useState, useReducer } from 'react';
-import { AppContext } from '../../context';
-import { reducer } from '../../reducer';
+import { useState } from 'react';
 
 import { ThemeProvider } from '@mui/material';
 import { lightTheme, darkTheme } from '../../themes';
@@ -24,26 +22,23 @@ import Projects from '../Reports/Projects';
 function App() {
     const [isModeDark, setIsModeDark] = useState(false);
     const location = useLocation();
-    const [state, dispatch] = useReducer(reducer, AppContext);
     return(
-        <AppContext.Provider value={{ state, dispatch }}>
-            <ThemeProvider theme={isModeDark ? darkTheme : lightTheme}>
-            {location.pathname === '/login' || location.pathname ==='/registration' ? null : 
-                <Navbar 
-                isModeDark={isModeDark} 
-                setIsModeDark={setIsModeDark} />}
-                <Routes>
-                    <Route path='login' element={<Login />} />
-                    <Route path='registration' element={<Registration />} />
-                    <Route path='dashboard' element={<Dashboard />} />
-                    <Route path='newbug' element={<BugForm />} />
-                    <Route path='newproject' element={<ProjectForm />} />
-                    <Route path='bugreports' element={<Bugs />} />
-                    <Route path='projectreports' element={<Projects />} />
-                    <Route path='*' element={<Navigate to='login' />} />
-                </Routes>
-            </ThemeProvider>
-        </AppContext.Provider>
+        <ThemeProvider theme={isModeDark ? darkTheme : lightTheme}>
+        {location.pathname === '/login' || location.pathname ==='/registration' ? null : 
+            <Navbar 
+            isModeDark={isModeDark} 
+            setIsModeDark={setIsModeDark} />}
+            <Routes>
+                <Route path='login' element={<Login />} />
+                <Route path='registration' element={<Registration />} />
+                <Route path='dashboard' element={<Dashboard />} />
+                <Route path='newbug' element={<BugForm />} />
+                <Route path='newproject' element={<ProjectForm />} />
+                <Route path='bugreports' element={<Bugs />} />
+                <Route path='projectreports' element={<Projects />} />
+                <Route path='*' element={<Navigate to='login' />} />
+            </Routes>
+        </ThemeProvider>
     )
 }
 
