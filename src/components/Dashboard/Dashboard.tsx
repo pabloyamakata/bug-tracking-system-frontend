73c5df97b66ed2b0697dad917d1428acfd619da9
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-03-04 02:53:28 -0300

    ...

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,13 +1,65 @@
-function Dashboard() {
-    return(
-        <h3 style={{paddingTop: '70px'}}>Here goes Dashboard component!</h3>
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-        // Perhaps Chart.js library should be used in order to
-        // display information related to bugs. For example,
-        // a doughnut chart that shows pending and solved bugs or,
-        // a pie chart which renders different types of bugs:
-        // high priority - low severity, low priority - high 
-        // severity and so on... 
+import { CustomPaper } from './DashboardStyles';
+import Typography from '@mui/material/Typography';
+
+const bugs_URL = 'http://localhost/bug-tracker-backend/bugs.php';
+const projects_URL = 'http://localhost/bug-tracker-backend/projects.php';
+const usernameURL = 'http://localhost/bug-tracker-backend/username.php';
+
+interface BugInterface {
+    id: number;
+    name: string;
+    description: string;
+    project: string;
+    project_leader: string;
+    current_status: string;
+    priority_level: string;
+    severity_level: string;
+    initial_date: Date;
+    final_date: Date;
+}
+
+interface ProjectInterface {
+    id: number;
+    name: string;
+    description: string;
+    project_leader: string;
+    start_date: Date;
+    deadline: Date;
+    current_status: string;
+    frontend: string;
+    backend: string;
+    ddbb: string;
+}
+
+function Dashboard() {
+    const [bugArray, setBugArray] = useState<BugInterface[]>([]);
+    const [projectArray, setProjectArray] = useState<ProjectInterface[]>([]);
+    const [userData, setUserData] = useState({ username: 'username' });
+
+    useEffect(() => {
+        const promiseForBugs = axios.get(bugs_URL, { withCredentials: true });
+        const promiseForProjects = axios.get(projects_URL, { withCredentials: true });
+        const promiseForUsername = axios.get(usernameURL, { withCredentials: true });
+
+        Promise.all([promiseForBugs, promiseForProjects, promiseForUsername])
+        .then(res => {
+            setBugArray(res[0].data);
+            setProjectArray(res[1].data);
+            setUserData(res[2].data);
+        });
+    }, []);
+
+    return(
+        <CustomPaper elevation={0}>
+            <Typography
+            variant='h5' 
+            sx={{marginLeft: '20px', paddingTop: '25px'}}>
+                Welcome {userData.username}!
+            </Typography>
+        </CustomPaper>
     )
 }
 
--- src/components/Dashboard/DashboardStyles.ts
+++ src/components/Dashboard/DashboardStyles.ts
@@ -1,1 +1,15 @@
-export {};
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material';
+
+export const CustomPaper = styled(Paper)({
+    minHeight: '100vh',
+    paddingTop: '65px',
+    paddingBottom: '30px',
+    borderRadius: 0,
+    '@media (max-width: 599px)': {
+        paddingTop: '58px'
+    },
+    '@media (min-width: 600px) and (max-width: 899px)': {
+        paddingTop: '64px'
+    }
+});
